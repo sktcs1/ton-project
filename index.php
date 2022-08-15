a902b27622d35508da1cc7a76f8b5d5176cc1757
sktcs1 <6240207123@nrru.ac.th>
2022-08-15 12:15:27 +0700

    add index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php 
+    echo "tes tes tes test test"; 
+    echo "tes tes asdfasdf test test"; 
+    echo "tes tes asdfasdf test test"; 
+    echo "tes asdf tes test test"; 
+    echo "tes tes tes test test"; 
+    echo "tes tes tes test asdfsdaf"; 
+?>
